26b916064194b8566fc472260618a79ed3065c50
Deborah Wang <dqw@sfu.ca>
2024-12-09 18:33:35 -0800

    header styles implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,58 +33,10 @@ export default function Home() {
                 </div>
 
                 <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
+                    <h1>Apparat Medium 96px / 84px with -2% tracking</h1>
+                    <h2>Apparat Medium 57.5px / 56px with -1.5% tracking</h2>
+                    <h3>GT Planar Regular 39.5px /42px with -1% tracking</h3>
                 </div>
-
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
-                </div>
-
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
-                </div>
-
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
-                </div>
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
-                </div>
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
-                </div>
-
-
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
             </footer>
