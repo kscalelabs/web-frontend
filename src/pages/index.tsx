ad5b82398d5c910f77a739fc9072bec3f2ed633a
Deborah Wang <dqw@sfu.ca>
2024-12-09 19:08:31 -0800

    update remaining types with responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,10 +32,22 @@ export default function Home() {
                     Butter
                 </div>
 
-                <div className="flex flex-col gap-4 text-2xl">
-                    <h1>Apparat Medium 96px / 84px with -2% tracking</h1>
-                    <h2>Apparat Medium 57.5px / 56px with -1.5% tracking</h2>
-                    <h3>GT Planar Regular 39.5px /42px with -1% tracking</h3>
+                <div className="flex flex-col gap-4 text-2xl items-start">
+                    <h1>H1: Apparat Medium 96px / 84px with -2% tracking</h1>
+                    <h2>h2: Apparat Medium 57.5px / 56px with -1.5% tracking</h2>
+                    <h3>h3: GT Planar Regular 39.5px /42px with -1% tracking</h3>
+                    <p>Body: GT Planar 24px / 28px -1% tracking</p>
+                    <figcaption>
+                        Figcaption, caption: GT Planar 17px / 28px 2% tracking
+                    </figcaption>
+
+                    <blockquote>
+                        <pre>
+                            <code>
+                                Blockquote, pre, code: CoFo Mono 18px / 18px 2% tracking
+                            </code>
+                        </pre>
+                    </blockquote>
                 </div>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
