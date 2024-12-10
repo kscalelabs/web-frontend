c03842f3d7916a252395c2f23455e18d2b698859
Deborah Wang <dqw@sfu.ca>
2024-12-10 04:28:19 -0800

    remove unused interfaces

--- src/components/footer/socialMediaSvgs.tsx
+++ src/components/footer/socialMediaSvgs.tsx
@@ -1,5 +1,3 @@
-import { SvgKeyProp } from "@/components/util/interfaces";
-
 export const Discord = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
