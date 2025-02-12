b8d9c2cd642f315522b8285add26519b041b8490
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-11 19:48:05 -0800

    feat: add robot model + fix font sizes & wrapping

--- src/landing/CatchphraseSection.tsx
+++ src/landing/CatchphraseSection.tsx
@@ -1,8 +1,13 @@
 const CatchphraseSection = () => {
   return (
     <section className="col-span-full flex items-start flex-col min-h-[50svh] md:min-h-[90svh] justify-center auto-rows-auto text-foreground bg-background relative overflow-hidden -mx-[5vw] px-[5vw]">
-      <h3 className="text-2xl max-w-sm">
-        We build open-source humanoid robots to move humanity up the Kardashev scale.
+      <h3 className="max-w-sm text-heading-md">
+        We build open-source
+        <br className="hidden md:inline" /> humanoid robots to move
+        <br className="hidden md:inline" />
+        humanity up the Kardashev
+        <br className="hidden md:inline" />
+        scale.
       </h3>
     </section>
   );
