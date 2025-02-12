6bb044d995bb634440abb36bea864c7143826738
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-11 16:36:46 -0800

    feat: made initial shift to responsive design

--- src/landing/CatchphraseSection.tsx
+++ src/landing/CatchphraseSection.tsx
@@ -1,9 +1,6 @@
-import { FillMode } from "@/components/util/constants";
-import { CTASubtitleButton } from "@/components/buttons/CTAButtons";
-
 const CatchphraseSection = () => {
   return (
-    <section className="col-span-full flex items-start flex-col min-h-[90svh] justify-center auto-rows-auto text-foreground bg-background relative overflow-hidden -mx-[5vw] px-[5vw]">
+    <section className="col-span-full flex items-start flex-col min-h-[50svh] md:min-h-[90svh] justify-center auto-rows-auto text-foreground bg-background relative overflow-hidden -mx-[5vw] px-[5vw]">
       <h3 className="text-2xl max-w-sm">
         We build open-source humanoid robots to move humanity up the Kardashev scale.
       </h3>
