b1b14b6fe5ede4084a5826c8f30838b973b4f384
Kevin Thomas <kevin.jt2007@gmail.com>
2025-02-13 15:50:25 -0800

    fix: blog mobile font size + order of name/reading time/date

--- src/pages/research/[slug].tsx
+++ src/pages/research/[slug].tsx
@@ -1,12 +1,12 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remarkMath from "remark-math";
 import readingTime from "reading-time";
+import rehypeKatex from "rehype-katex";
 import { MDXRemote } from "next-mdx-remote";
 import Layout from "../../components/Layout";
 import { serialize } from "next-mdx-remote/serialize";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 
 const RESEARCH_PATH = path.join(process.cwd(), "src/content/research");
 
@@ -43,17 +43,19 @@ export async function getStaticProps({ params }: { params: { slug: string } }) {
 export default function ResearchPost({ source, frontMatter }: { source: any; frontMatter: any }) {
   return (
     <Layout>
-      <header className="flex flex-col w-[90%] mx-auto gap-4 items-start justify-end pb-10 min-h-[60svh]">
-        <h1 className="!text-3xl !m-0 !font-medium">{frontMatter.title}</h1>
-        <div className="flex items-center w-full justify-between text-xs">
-          <div className="flex items-center gap-4">
-            <p>{frontMatter.author}</p>
-            <p>{frontMatter.date}</p>
+      <header className="flex flex-col w-[90%] mx-auto gap-2 items-start justify-end pb-10 min-h-[60svh]">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center w-full justify-between text-xs">
+            <p className="!m-0 !text-sm">{frontMatter.date}</p>
+            <p className="text-foreground70 !m-0 !text-sm">{frontMatter.readingTime}</p>
           </div>
-          <p className="text-foreground70">{frontMatter.readingTime}</p>
+          <h1 className="!text-3xl !m-0 !font-medium">{frontMatter.title}</h1>
+        </div>
+        <div className="flex items-center w-full justify-between text-xs">
+          <p>{frontMatter.author}</p>
         </div>
       </header>
-      <article className="mdx-content w-[90%] mx-auto !p-0">
+      <article className="mdx-content w-[90%] *:!text-[100%] mx-auto !p-0">
         <MDXRemote {...source} />
       </article>
     </Layout>
