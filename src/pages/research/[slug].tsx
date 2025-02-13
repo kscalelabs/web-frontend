c85c17351a98283286f8f3cb9f860f81e4579284
budzianowski <budzianowski@gmail.com>
2025-02-12 19:14:52 -0800

    rebase

--- src/pages/research/[slug].tsx
+++ src/pages/research/[slug].tsx
@@ -36,73 +36,17 @@ export async function getStaticProps({ params }: { params: { slug: string } }) {
 export default function ResearchPost({ source, frontMatter }: { source: any; frontMatter: any }) {
   return (
     <Layout>
-      <style jsx global>{`
-        .mdx-content p {
-          line-height: 1.5 !important;
-          text-align: left;
-        }
-
-        .mdx-content img {
-          border-radius: 0px !important;
-        }
-
-        .mdx-content h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          text-align: left !important;
-        }
-
-        .mdx-content h1 {
-          font-size: 2rem !important;
-          margin-top: 1.5rem !important;
-          margin-bottom: 1rem !important;
-        }
-
-        .mdx-content h2 {
-          font-size: 1.75rem !important;
-          margin-top: 1.5rem !important;
-          margin-bottom: 1rem !important;
-        }
-
-        .mdx-content h3 {
-           font-size: 1.5rem !important;
-           margin-top: 1.5rem !important;
-           margin-bottom: 1rem !important;
-         }
-
-         .mdx-content h4 {
-           font-size: 1.25rem !important;
-           margin-top: 1.5rem !important;
-           margin-bottom: 1rem !important;
-         }
-
-         .mdx-content h5 {
-           font-size: 1.125rem !important;
-           margin-top: 1.5rem !important;
-           margin-bottom: 1rem !important;
-         }
-
-         .mdx-content h6 {
-           font-size: 1rem !important;
-           margin-top: 1.5rem !important;
-           margin-bottom: 1rem !important;
-         }
-      }
-      `}</style>
-      <header className="flex flex-col w-full gap-4 items-start justify-end pb-10 min-h-[60svh]">
+      <header className="flex flex-col w-[90%] mx-auto gap-4 items-start justify-end pb-10 min-h-[60svh]">
         <h1 className="!text-3xl !m-0 !font-medium">{frontMatter.title}</h1>
         <div className="flex items-center w-full justify-between text-xs">
           <div className="flex items-center gap-4">
             <p>{frontMatter.author}</p>
             <p>{frontMatter.date}</p>
           </div>
-          <p className="text-foreground70">{frontMatter.readingTime}</p>
+          <p className="text-foreground70">{frontMatter.readingTime}</p>  
         </div>
       </header>
-      <article className="mdx-content mx-auto !p-0">
+      <article className="mdx-content w-[90%] mx-auto !p-0">
         <MDXRemote {...source} />
       </article>
     </Layout>
