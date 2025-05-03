074aa30bb2c3cdab2f578fc1f8f943cd7ed5f790
Benjamin Bolte <62453249+chrisangelothomas@users.noreply.github.com>
2025-05-03 02:02:25 -0700

    Updated Copy and color

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -24,8 +24,9 @@ export default function Page() {
   return (
     <>
       <section className="h-[80svh] px-layout min-h-screen py-8">
+
         <hgroup className="mb-16">
-          <h1 className="text-body-3 mb-2">Careers</h1>
+          <h1 className="text-body-3 text-stone-400 mb-2">Careers</h1>
           <h2 className="mb-4">Open roles (3)</h2>
           <h3 className="mb-4">All roles are on-site in Palo Alto, CA, USA. </h3>
         </hgroup>
@@ -40,7 +41,7 @@ export default function Page() {
               >
                 <div>
                   <h3 className="font-bold text-body-2 mb-1">{item.title}</h3>
-                  <p className="text-body-3 text-stone-500">{item.description}</p>
+                  <p className="text-body-3 text-stone-400">{item.description}</p>
                 </div>
                 <Arrow className="size-8 group-hover:translate-x-1/4 group-hover:-translate-y-1/4 group-focus:translate-x-1/4 group-focus:-translate-y-1/4 transition-transform duration-300" />
               </a>
@@ -51,6 +52,14 @@ export default function Page() {
           <p className="mb-4"> Don't see an role that matches you?</p>
           <CopyButton />
         </aside>
+        <div className="h-16" />
+        <div className="mb-16 w-full lg:w-1/2">
+          <p className="text-body-3 text-stone-400 mb-2">People of K-Scale</p>
+          <h2 className="mb-4">Who we are looking for?</h2>
+          <p className="text-body-2">
+            We don't care about degrees or traditional backgrounds. We care about people who can independently ship top-tier open-source humanoid robots. Show us your GitHub, open-source work, standout projects, or competition wins.
+          </p>
+        </div>
       </section>
     </>
   );
