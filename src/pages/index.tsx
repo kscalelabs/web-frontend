72f645a6dfdb83ef14608f601ce0693eb76ae6ab
nethen <72537484+nethen@users.noreply.github.com>
2025-05-03 02:22:37 -0700

    feat: products section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,47 @@ export default function Home() {
           </p>
           <Button fullWidth>View community benchmarks</Button>
         </section>
+        <section className="px-layout flex flex-col py-16 ">
+          <hgroup className="col-span-full mb-4">
+            <h2 className="text-body-2 font-medium text-stone-400 mb-1">Products</h2>
+            <h3 className="mb-4">Humanoid robots for all your research and development needs</h3>
+          </hgroup>
+          <ul className="flex flex-col gap-8 md:gap-6 md:grid grid-cols-2 lg:grid-cols-3">
+            <li>
+              <article>
+                <div className="aspect-[3/4] sm:aspect-video bg-gradient-to-br from-rust via-background to-methyl rounded-2xl mb-4" />
+                <h4 className="text-heading-1">K-Bot</h4>
+                <h5 className="text-heading-2 mb-1">Full sized humanoid</h5>
+                <p className="text-stone-400 mb-4">Starting at $9,000 USD</p>
+                <Button href="https://shop.kscale.dev" external>
+                  Pre-order now
+                </Button>
+              </article>
+            </li>
+            <li>
+              <article>
+                <div className="aspect-[3/4] sm:aspect-video bg-gradient-to-br from-rust via-background to-methyl rounded-2xl mb-4" />
+                <h4 className="text-heading-1">Z-Bot</h4>
+                <h5 className="text-heading-2 mb-1">Small but mighty</h5>
+                <p className="text-stone-400 mb-4">Starting at $1,000 USD</p>
+                <Button href="https://zeroth.bot" external>
+                  Buy now
+                </Button>
+              </article>
+            </li>
+            <li>
+              <article>
+                <div className="aspect-[3/4] sm:aspect-video bg-gradient-to-br from-rust via-background to-methyl rounded-2xl mb-4" />
+                <h4 className="text-heading-1 flex items-center gap-2">
+                  M-Bot{" "}
+                  <span className="text-stone-500 text-body-3 font-bold border border-stone-500 rounded-full px-3 py-1">
+                    Coming soon
+                  </span>
+                </h4>
+              </article>
+            </li>
+          </ul>
+        </section>
         <section className="px-layout flex flex-col grid-r py-16 bg-stone-950">
           <hgroup className="col-span-full mb-4">
             <h2 className="text-body-2 font-medium text-stone-400 mb-1">Demos</h2>
@@ -53,7 +94,18 @@ export default function Home() {
             <p>An impressive statistic about the current state of locomotion.</p>
           </article>
         </section>
-        <section className="h-[200svh] bg-white" />
+        <section className="px-layout flex flex-col grid-r py-16 ">
+          <hgroup className="col-span-full mb-4">
+            <h2 className="text-body-2 font-medium text-stone-400 mb-1">Our achievements</h2>
+            <h3 className="mb-4">
+              We&apos;ve completed 3 generations of robots in less than a year
+            </h3>
+          </hgroup>
+          <article className="col-span-full 2xl:col-span-6 2xl:col-start-2 aspect-[3/4] sm:aspect-video bg-gradient-to-br from-rust via-background to-methyl rounded-2xl p-6 flex flex-col justify-end">
+            <p>An impressive statistic about the current state of locomotion.</p>
+          </article>
+        </section>
+
         {/* <section className="px-5 md:px-10">
           <h1 className="mb-4">K-Scale Labs</h1>
           <h2 className="mb-4">The new frontier</h2>
