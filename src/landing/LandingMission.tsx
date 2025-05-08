4556a57ce1188dc000a59361bf0d1f5a2501634c
JX <jingxiangmo@gmail.com>
2025-05-08 12:47:56 -0700

    fix: mission writing

--- src/landing/LandingMission.tsx
+++ src/landing/LandingMission.tsx
@@ -1,15 +1,20 @@
 export function LandingMission() {
   return (
     <section className="section">
-      <hgroup className="col-span-default col-start-default mb-6">
-        <h2 className="text-heading-1 mb-2">
-          We&apos;re building the most integrated ML robotics platform
+      <div className="col-span-default col-start-default mb-8">
+        <h2 className="text-heading-1 mb-4">
+          The most integrated ML & hardware robotics stack
         </h2>
-        <p className="text-body-1 mb-6">
-          Count on us to deliver cutting-edge tech, built on high-community standards from
-          robust hardware and firmware to machine learning models.
+        <p className="text-body-1">
+          We own the entire stack, from shipping state of the art machine learning models trained on our infrastructure, building the operating system, designing the hardware, and manufacturing it.
         </p>
-      </hgroup>
+      </div>
+      
+      <div className="col-span-default col-end-default col-start-7 mb-6">
+        <p className="text-body-1">
+          Our mission to solve general purpose robotics and open-sourcing it to everyone.
+        </p>
+      </div>
     </section>
   );
 } 
