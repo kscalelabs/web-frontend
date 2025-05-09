e899b39d0c7e70ecc05da1c0001cbeb47573ceee
JX <jingxiangmo@gmail.com>
2025-05-08 17:56:50 -0700

    fix lint

---
+++ src/pages/benchmarks/index.tsx
@@ -0,0 +1,8 @@
+export default function BenchmarksPage() {
+  return (
+    <div className="px-layout py-16">
+      <h1 className="text-heading-2 mb-4">Benchmarks</h1>
+      <p className="text-body-2 text-stone-200">Benchmarks data will be available soon.</p>
+    </div>
+  );
+}
