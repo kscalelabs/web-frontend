c11b69e9b83c21464f03d1df79fa8a0c51feabaf
nethen <72537484+nethen@users.noreply.github.com>
2025-05-12 01:01:27 -0700

    fix: landing video

--- src/landing/LandingHero.tsx
+++ src/landing/LandingHero.tsx
@@ -14,7 +14,7 @@ export function LandingHero() {
           playsInline
           className="absolute inset-0 size-full object-cover brightness-[0.25]"
         >
-          <source src="/videos/landing/landing_video_0506.mp4" type="video/mp4" />
+          <source src="/videos/landing/landing_video_0512.mp4" type="video/mp4" />
         </video>
         <hgroup className="col-span-4 2xl:col-start-1 2xl:col-span-4 z-10">
           <h1 className="mb-4">Open-source humanoid robots, built for developers</h1>
