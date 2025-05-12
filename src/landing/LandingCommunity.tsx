d44709d2067283637fa023a98148f07c63985ee3
nethen <72537484+nethen@users.noreply.github.com>
2025-05-12 01:35:38 -0700

    fix: community photos placement

--- src/landing/LandingCommunity.tsx
+++ src/landing/LandingCommunity.tsx
@@ -27,7 +27,7 @@ export const LandingCommunity = () => {
             </a>
           </div>
         </hgroup>
-        <figure className="col-span-full sm:col-span-2 lg:col-span-4 2xl:col-start-2 aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
+        <figure className="col-span-full sm:col-span-2 lg:col-span-3 lg:col-start-1 2xl:col-span-2 2xl:col-start-2 4xl:col-span-4 4xl:col-start-2 aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
           <Image
             src={"/photos/landing/community/Team.webp"}
             alt={"Our team working on K-Bot V2"}
@@ -38,7 +38,7 @@ export const LandingCommunity = () => {
             sizes={"100dvw"}
           />
         </figure>
-        <figure className="col-span-full sm:col-span-2 aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
+        <figure className="col-span-full sm:col-span-2 lg:col-span-3 2xl:col-span-2 4xl:col-span-2 aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
           <Image
             src={"/photos/landing/community/Hackathon_Demo_2.webp"}
             alt={"Hackathon demos at K-Scale Labs"}
@@ -49,7 +49,7 @@ export const LandingCommunity = () => {
             sizes={"100dvw"}
           />
         </figure>
-        <figure className="col-span-full sm:col-span-2 sm:col-start-2 lg:col-span-4 lg:col-start-2 2xl:col-start-3  aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
+        <figure className="col-span-full sm:col-span-2 sm:col-start-2 lg:col-span-4 lg:col-start-2 2xl:col-start-3 2xl:col-span-2 4xl:col-start-3 4xl:col-span-4  aspect-square sm:aspect-video rounded-lg relative overflow-hidden mb-4 md:mb-6">
           <Image
             src={"/photos/landing/community/HackathonDemos.webp"}
             alt={"Hackathon demos at K-Scale Labs"}
