420dd8c1ce721a2f69b8583be6edf2c10cf47663
nethen <72537484+nethen@users.noreply.github.com>
2025-05-13 13:37:03 -0700

    fix: mission statement alignment

--- src/landing/LandingMission.tsx
+++ src/landing/LandingMission.tsx
@@ -1,20 +1,56 @@
 export function LandingMission() {
   return (
-    <section className="section">
+    <section className="section pb-0">
       <div className="section-container">
-        <h2 className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 lg:col-start-2 2xl:col-span-5 2xl:col-start-3 4xl:col-span-6 4xl:col-start-5 text-heading-1 mb-6">
-          We&apos;re building a robotics stack that lets you deploy general-purpose AI in the real
-          world
-        </h2>
-        <p className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 lg:col-start-2 2xl:col-span-5 2xl:col-start-3 4xl:col-span-6 4xl:col-start-5 text-body-1 mb-12">
-          We design and manufacture the hardware, develop a custom operating system, and train
-          machine learning models on our own modular framework.
-        </p>
-        <p className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-5 4xl:col-span-6 sm:-col-end-1 md:-col-end-1 lg:-col-end-2 2xl:-col-end-3 4xl:-col-end-5 text-body-1 mb-6">
+        <hgroup className="col-span-full sm:col-span-4 md:col-span-4 md:col-start-2 lg:col-span-3 2xl:col-span-5 2xl:col-start-3 4xl:col-span-6 4xl:col-start-4">
+          <h2 className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 lg:col-start-2 2xl:col-span-5 2xl:col-start-3 4xl:col-span-6 4xl:col-start-4 text-heading-1 mb-6">
+            We&apos;re building a robotics stack that lets you deploy general-purpose AI in the real
+            world
+          </h2>
+          <p className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-5 2xl:col-start-3 4xl:col-span-6 4xl:col-start-4 text-body-1 mb-12">
+            We design and manufacture the hardware, develop a custom operating system, and train
+            machine learning models on our own modular framework.
+          </p>
+        </hgroup>
+        <figure className="w-full col-span-full sm:col-span-4 md:col-span-4 lg:col-span-3 2xl:col-span-3 4xl:col-span-4 sm:-col-end-1 md:-col-end-2 2xl:-col-end-3 4xl:-col-end-4 aspect-video 2xl:aspect-video rounded-lg bg-stone-800 mb-64">
+          {/* <svg
+            className="size-full"
+            viewBox="0 0 256 256"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.5"
+              d="M119.928 30.8829C125.148 29.0186 130.852 29.0186 136.072 30.8829L229.452 64.233C232.992 65.4972 232.992 70.5028 229.452 71.767L136.072 105.117C130.852 106.981 125.148 106.981 119.928 105.117L26.5475 71.767C23.008 70.5028 23.008 65.4972 26.5475 64.233L119.928 30.8829Z"
+              fill="#DE4208"
+            />
+            <path
+              opacity="0.5"
+              d="M119.928 17.9063C125.148 16.0421 130.852 16.0421 136.072 17.9063L173.452 31.2565C176.992 32.5206 176.992 37.5263 173.452 38.7904L136.072 52.1406C130.852 54.0048 125.148 54.0048 119.928 52.1406L82.5475 38.7904C79.008 37.5263 79.008 32.5206 82.5475 31.2565L119.928 17.9063Z"
+              fill="#DE4208"
+            />
+            <path
+              opacity="0.5"
+              d="M119.928 70.8829C125.148 69.0186 130.852 69.0186 136.072 70.8829L229.452 104.233C232.992 105.497 232.992 110.503 229.452 111.767L136.072 145.117C130.852 146.981 125.148 146.981 119.928 145.117L26.5475 111.767C23.008 110.503 23.008 105.497 26.5475 104.233L119.928 70.8829Z"
+              fill="#DE4208"
+            />
+            <path
+              opacity="0.5"
+              d="M119.928 110.883C125.148 109.019 130.852 109.019 136.072 110.883L229.452 144.233C232.992 145.497 232.992 150.503 229.452 151.767L136.072 185.117C130.852 186.981 125.148 186.981 119.928 185.117L26.5475 151.767C23.008 150.503 23.008 145.497 26.5475 144.233L119.928 110.883Z"
+              fill="#DE4208"
+            />
+            <path
+              opacity="0.5"
+              d="M119.928 163.859C125.148 161.995 130.852 161.995 136.072 163.859L180.156 179.604C183.695 180.868 183.695 185.873 180.156 187.138L136.072 202.882C130.852 204.746 125.148 204.746 119.928 202.882L75.8444 187.138C72.3049 185.873 72.3048 180.868 75.8444 179.604L119.928 163.859Z"
+              fill="#DE4208"
+            />
+          </svg> */}
+        </figure>
+        {/* <p className="col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-5 4xl:col-span-6 sm:-col-end-1 md:-col-end-1 lg:-col-end-2 2xl:-col-end-3 4xl:-col-end-4 text-heading-1 mb-6"> */}
+        <p className="col-span-full sm:col-span-4 md:col-start-2 2xl:col-span-6 2xl:col-start-4 4xl:col-span-8 4xl:col-start-5 text-heading-1 text-center">
           Owning every layer—from metal to model—lets us move faster, integrate more deeply, and
           open-source each breakthrough for the whole world to build on.
         </p>
-        {/* <figure className="bg-stone-900 w-full col-span-full sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-5 4xl:col-span-6 sm:-col-end-1 md:-col-end-1 lg:-col-end-2 2xl:-col-end-3 4xl:-col-end-5 aspect-video rounded-lg" /> */}
       </div>
     </section>
   );
