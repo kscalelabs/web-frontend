52aece4d0ef9cc251b8fdf43c15945b63ac7c33b
nethen <72537484+nethen@users.noreply.github.com>
2025-05-14 19:35:29 -0700

    feat: benchmarks content

--- src/pages/benchmarks/index.tsx
+++ src/pages/benchmarks/index.tsx
@@ -7,6 +7,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Arrow from "@/assets/icons/icon_arrowTR.svg";
+import { Button } from "@/components/ui/Button/Button";
+import Discord from "@/assets/icons/icon_discord.svg";
 
 interface Props {
   html: string[];
@@ -188,6 +190,141 @@ export default function Page({ html, raw }: Props) {
               <p>Track a 30-sec motion capture clip</p>
             </hgroup>
           </div>
+        </div>
+      </section>
+      <section className="section--short">
+        <div className="section-container">
+          <div className="section-prose2">
+            <h2 className="text-heading-1 col-span-default col-start-default mb-6">
+              How can I submit?
+            </h2>
+            <ol className="list-inside list-decimal text-body-1 mb-12">
+              <li className="mb-6">Export your policy to a K-Infer model</li>
+              <li className="mb-6">Evaluate in sim2sim and upload a Youtube video</li>
+              <li className="mb-6">
+                Upload your submission to our{" "}
+                <a
+                  href="https://google.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                >
+                  Discord
+                </a>
+              </li>
+              <li className="mb-6">Post the link in #benchmark-submission on Discord</li>
+            </ol>
+            <p className="mb-4">
+              One of us will run your policy on the real robot and add the results to the
+              leaderboard.
+            </p>
+            <p className="mb-4">
+              We try to run new policies every night, so you shouldn&apos;t have to wait too long to
+              see your policy on the leaderboard.
+            </p>
+            <p>
+              We will post your sim2sim and sim2real deployment video on X/Twitter, and on top of
+              passing basic, evaluation, your rank is determined by the number of likes on Twitter.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="section--short">
+        <div className="section-container">
+          <div className="section-prose2">
+            <h2 className="text-heading-1 col-span-default col-start-default mb-6">
+              Frequently asked questions
+            </h2>
+            <ul className="mb-12">
+              <li className="mb-6">
+                <h3 className="text-body-2 font-bold mb-2">What license should I use?</h3>
+                <p>
+                  MIT, as all of our{" "}
+                  <a
+                    href="https://github.com/kscalelabs"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    stack
+                  </a>
+                </p>
+              </li>
+              <li className="mb-6">
+                <h3 className="text-body-2 font-bold mb-2">Can I use use my own algorithm?</h3>
+                <p>
+                  Yes, you can! Check out our{" "}
+                  <a
+                    href="https://docs.kscale.dev/docs/task#/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    docs
+                  </a>{" "}
+                  for more details.
+                </p>
+              </li>
+              <li className="mb-6">
+                <h3 className="text-body-2 font-bold mb-2">Are you hiring?</h3>
+                <p>
+                  Yes, we are. We currently have{" "}
+                  <Link
+                    href="/careers"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    3 open roles
+                  </Link>{" "}
+                  for engineering.
+                </p>
+              </li>
+              <li className="mb-6">
+                <h3 className="text-body-2 font-bold mb-2">Where should I submit issues?</h3>
+                <p>
+                  Please submit GitHub issue at the source of the repos for{" "}
+                  <a
+                    href="https://github.com/kscalelabs/ksim"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    K-SIM
+                  </a>{" "}
+                  ,{" "}
+                  <a
+                    href="https://github.com/kscalelabs/ksim-gym"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    K-SIM Gym
+                  </a>{" "}
+                  , and{" "}
+                  <a
+                    href="https://github.com/kscalelabs/kinfer"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="underline text-orange-500 hover:text-orange-400 whitespace-nowrap font-medium transition-colors duration-300"
+                  >
+                    K-Infer
+                  </a>{" "}
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="section pt-8">
+        <div className="section-container">
+          <hgroup className="col-span-full lg:col-span-4 lg:col-start-2 2xl:col-span-4 4xl:col-span-6 2xl:col-start-5 4xl:col-start-6 flex flex-col items-center text-center">
+            <h2 className="text-heading-2 mb-6">
+              Have any questions? Send them our way into our community Discord!
+            </h2>
+
+            <Button href="https://discord.com/invite/pVwubQT9Sg" external icon={Discord}>
+              Join our Discord
+            </Button>
+          </hgroup>
         </div>
       </section>
     </main>
