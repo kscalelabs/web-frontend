5f29244b94bd3a4dfde0aa9eda701c4498836268
nethen <72537484+nethen@users.noreply.github.com>
2025-05-16 21:48:34 -0700

    fix: revert back to old landing

--- src/landing/LandingHero.tsx
+++ src/landing/LandingHero.tsx
@@ -1,42 +1,36 @@
 import { Button } from "@/components/ui/Button/Button";
-import BackgroundVideo from "next-video/background-video";
+import BackgroundVideo from "next-video";
+import Video from "next-video";
 import HeroVideo from "@/videos/landing/landing_video_0514.mp4";
 
 export function LandingHero() {
   return (
-    <section className="relative min-h-[20rem] h-[87.5svh] max-h-[1280px] overflow-hidden">
-      <BackgroundVideo className="min-h-[20rem] h-[87.5svh] max-h-[1280px]" src={HeroVideo}>
-        <div className="z-50 section-container h-full sm:content-end">
-          <hgroup className="col-span-4 md:col-span-5 xl:col-span-4 2xl:col-span-7 z-10">
-            <h1 className="mb-3">Open-source humanoid robots, built for developers</h1>
-            <p className="text-body-1 mb-6">
-              We&apos;re accelerating the timeline to a world with billions of robots, and making
-              sure they&apos;re accessible, auditable, and beneficial to humanity.
-            </p>
-            <Button adaptive={true} href="/benchmarks" size="lg">
-              Get started now
-            </Button>
-          </hgroup>
-          {/* </BackgroundVideo> */}
-        </div>
-      </BackgroundVideo>
+    <section className="py-8 relative min-h-[20rem] h-[87.5svh] max-h-[1280px] overflow-hidden">
+      <div className="section-container h-full justify-end sm:content-end">
+        <video
+          width="1920"
+          height="1080"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="absolute inset-x-0 -inset-y-2 size-full object-cover brightness-[0.4]"
+        >
+          <source src="/videos/landing/landing_video_0514.mp4" type="video/mp4" />
+        </video>
+
+        <hgroup className="col-span-4 md:col-span-5 xl:col-span-4 2xl:col-span-7 z-10">
+          <h1 className="mb-3">Open-source humanoid robots, built for developers</h1>
+          <p className="text-body-1 mb-6">
+            We&apos;re accelerating the timeline to a world with billions of robots, and making sure
+            they&apos;re accessible, auditable, and beneficial to humanity.
+          </p>
+          <Button adaptive={true} href="/benchmarks" size="lg">
+            Get started now
+          </Button>
+        </hgroup>
+        {/* </BackgroundVideo> */}
+      </div>
     </section>
   );
 }
-
-{
-  /* <BackgroundVideo src={HeroVideo}> */
-}
-<div className="z-50 section-container h-full sm:content-end">
-  <hgroup className="col-span-4 md:col-span-5 xl:col-span-4 2xl:col-span-7 z-10">
-    <h1 className="mb-3">Open-source humanoid robots, built for developers</h1>
-    <p className="text-body-1 mb-6">
-      We&apos;re accelerating the timeline to a world with billions of robots, and making sure
-      they&apos;re accessible, auditable, and beneficial to humanity.
-    </p>
-    <Button adaptive={true} href="/benchmarks" size="lg">
-      Get started now
-    </Button>
-  </hgroup>
-  {/* </BackgroundVideo> */}
-</div>;
